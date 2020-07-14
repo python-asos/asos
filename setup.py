c596140126231db68ccbcc63a2d867621718a98d
Pavel Kim <hello@pavelkim.ru>
2020-07-15 00:33:07 +0300

    [xChange] Rearranged for the pip's sake

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 import setuptools
+from version import version
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="asos",
-    version="1.0.0",
+    version=version,
     author="Pavel Kim",
     author_email="hello@pavelkim.ru",
     description="A scheduler of stuff (ASOS)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pavelkim/asos",
+    url="https://github.com/python-asos/asos/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
