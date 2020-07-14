ec30163acdd3933e78f1a480be3117ba8832873a
Pavel Kim <hello@pavelkim.ru>
2020-07-15 02:45:28 +0300

    [Fix] Missed args object

--- asos/__main__.py
+++ asos/__main__.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
 	parser.add_argument("--storage-plugin", "-s", dest="storage_plugin", metavar="plugin", required=True, type=str, help="Storage plugin name")
 	parser.add_argument("--instance-uuid", "-u", dest="instance_uuid", metavar="name", required=True, type=str, help="Current instance name to identify its results in storage")
 	args = parser.parse_args(sys.argv[1:])
-	main(storage_plugin=args.storage_plugin, instance_uuid=instance_uuid)
+	main(storage_plugin=args.storage_plugin, instance_uuid=args.instance_uuid)
